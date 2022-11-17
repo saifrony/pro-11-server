ddf492326f6bf181b107e4d5a8d68b7fa8671dc0
saifRony <isaif326@gmail.com>
2022-11-18 02:49:37 +0600

    mongo

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cust1.baig3hx.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://visa:Uyhv7q8H9rKXp78S@cust1.baig3hx.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
